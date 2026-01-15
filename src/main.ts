bf225fc62ef7f3cbba4acd9b50ee176495fc5a9e
iwant <53087594+1want@users.noreply.github.com>
2026-01-15 16:14:13 +0800

    添加格式化规则

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import './assets/css/reset.css'
+// import 'katex/dist/katex.min.css'
 
 createApp(App).use(router).mount('#app')
